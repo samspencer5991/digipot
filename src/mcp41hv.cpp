3871ba7f8402a727a20c2a5ac0cdcab8c728872f
Sam Spencer <sam.spencer5991@gmail.com>
2025-01-16 14:00:37 +1100

    Update

--- src/mcp41hv.cpp
+++ src/mcp41hv.cpp
@@ -1,22 +1,46 @@
 #include "mcp41hv.h"
-#include "Arduino.h"
 
+#ifdef USE_MCP41HV_DIGIPOT
+#include "SPI.h"
+#include <api/HardwareSPI.h>
+#include <Arduino.h>
 #define MCP41HV_CMD_WRITE 			0x00	// 8-bit command to write to MCPHV41 Write address
 #define MCP41HV_CMD_READ 			0x0f	// 8-bit command to write to MCPHV41 Read address
 #define MCP41HV_CMD_INCREMENT 	0x04	// 8-bit command to write to MCPHV41 Increment address
 #define MCP41HV_CMD_DECREMENT 	0x08	// 8-bit command to write to MCPHV41 Decrement address
+void digipot_SetPinState(Mcp41hvDigipot* digipot, uint8_t state);
+#endif
 
-#define TEST_VALUE				69	// Used for verification of digipot wiper setting
-#define SPI_FREQUENCY			1000000
-#define SPI_BUS					SPI1
 
-void mcp41hv_Write(Mcp41hvDigipot* mcp41hv, uint8_t data)
+/*********** MCP45HV *********/
+
+/**
+  * @brief	This function takes a pointer to an mcp41hv type digipot type and performs a write/read check
+  * 		The outcome of this check is reflected in the digipot->status value
+  * @param 	pointer to the digipot to initialise
+  * @retval	none
+  */
+#ifdef USE_MCP41HV_DIGIPOT
+/**
+  * @brief 	Writes to the volatile wiper address of MCP41HV digipots
+  * @param 	data data to be written
+  * @retval data read from SDO pin
+  */
+void mcp41hv_write(Mcp41hvDigipot* digipot, uint8_t data)
 {
 	// prepare data buffer
 	uint8_t txData[2] = {MCP41HV_CMD_WRITE, data};
-	SPI_BUS.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, SPI_MODE0));
-	SPI_BUS.transfer(&txData, 2);
-	SPI_BUS.endTransaction();
+#if defined(FRAMEWORK_STM32CUBE)
+	// transmit decrement
+	if(HAL_SPI_Transmit_IT(digipot->hspi, &txData, 2)  != HAL_OK)
+	{
+		digipot->status = DigipotHalError;
+	}
+#elif defined(FRAMEWORK_ARDUINO)
+	digipot->hspi->beginTransaction(SPISettings(1330000, MSBFIRST, SPI_MODE0));
+	digipot->hspi->transfer(&txData, 2);
+	digipot->hspi->endTransaction();
+#endif
 }
 
 /**
@@ -25,16 +49,28 @@ void mcp41hv_Write(Mcp41hvDigipot* mcp41hv, uint8_t data)
   * @retval data read from SDO pin
   */
 
-uint8_t mcp41hv_Read(Mcp41hvDigipot* mcp41hv)
+uint8_t mcp41hv_read(Mcp41hvDigipot* digipot)
 {
 	// dummy data in txData to
 	uint8_t txData[2] = {MCP41HV_CMD_READ, 0x00};
 	uint8_t rxData[2];
 	// transmit data
-	SPI_BUS.beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, SPI_MODE0));
-	rxData[0] = SPI_BUS.transfer(txData[0]);
-	rxData[1] = SPI_BUS.transfer(txData[1]);
-	SPI_BUS.endTransaction();
+#if defined(FRAMEWORK_STM32CUBE)
+	if(HAL_SPI_TransmitReceive(digipot->hspi, txData, rxData, 2, 10) != HAL_OK)
+	{
+		digipot->status = DigipotHalError;
+	}
+#elif defined(FRAMEWORK_ARDUINO)
+	digipot->hspi->beginTransaction(SPISettings(1330000, MSBFIRST, SPI_MODE0));
+	rxData[0] = digipot->hspi->transfer(txData[0]);
+	rxData[1] = digipot->hspi->transfer(txData[1]);
+	digipot->hspi->endTransaction();
+#endif
+	// check data to ensure valid command
+	if(rxData[0] != 0xff)
+	{
+		digipot->status = DigipotCmdError;
+	}
 	return rxData[1];
 }
 
@@ -43,13 +79,20 @@ uint8_t mcp41hv_Read(Mcp41hvDigipot* mcp41hv)
   * @param	*digipot Pointer to the digipot type to use
   * @retval none
   */
-void mcp41hv_increment(Mcp41hvDigipot* mcp41hv)
+void mcp41hv_increment(Mcp41hvDigipot* digipot)
 {
 	uint8_t cmd = MCP41HV_CMD_INCREMENT;
-
-	mcp41hv->hspi->beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, SPI_MODE0));
-	mcp41hv->hspi->transfer(&cmd, 1);
-	mcp41hv->hspi->endTransaction();
+#if defined(FRAMEWORK_STM32CUBE)
+	// transmit decrement
+	if(HAL_SPI_Transmit_IT(digipot->hspi, &cmd, 1)  != HAL_OK)
+	{
+		digipot->status = DigipotHalError;
+	}
+#elif defined(FRAMEWORK_ARDUINO)
+	digipot->hspi->beginTransaction(SPISettings(1330000, MSBFIRST, SPI_MODE0));
+	digipot->hspi->transfer(&cmd, 1);
+	digipot->hspi->endTransaction();
+#endif
 }
 
 /**
@@ -57,11 +100,19 @@ void mcp41hv_increment(Mcp41hvDigipot* mcp41hv)
   * @param 	*digipot Pointer to the digipot type to use
   * @retval	none
   */
-void mcp41hv_decrement(Mcp41hvDigipot* mcp41hv)
+void mcp41hv_decrement(Mcp41hvDigipot* digipot)
 {
 	uint8_t cmd = MCP41HV_CMD_DECREMENT;
-
-	mcp41hv->hspi->beginTransaction(SPISettings(SPI_FREQUENCY, MSBFIRST, SPI_MODE0));
-	mcp41hv->hspi->transfer(&cmd, 1);
-	mcp41hv->hspi->endTransaction();
+#if defined(FRAMEWORK_STM32CUBE)
+	// transmit decrement
+	if(HAL_SPI_Transmit_IT(digipot->hspi, &cmd, 1)  != HAL_OK)
+	{
+		digipot->status = DigipotHalError;
+	}
+#elif defined(FRAMEWORK_ARDUINO)
+	digipot->hspi->beginTransaction(SPISettings(1330000, MSBFIRST, SPI_MODE0));
+	digipot->hspi->transfer(&cmd, 1);
+	digipot->hspi->endTransaction();
+#endif
 }
+#endif
